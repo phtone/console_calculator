53b866d33f71ecaf0e47a2c15528c2bf866c6f33
phtone <phtone@gmail.com>
2013-09-05 23:19:34 +0400

    add

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numberOne, numberTwo;
+        String sign;
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Введите первое число ");
+
+        numberOne = read.nextInt();
+
+        System.out.println("Вы ввели число: " + numberOne);
+
+        System.out.println("Введите знак операции ");
+
+        sign = read.next();
+
+        char v = sign.charAt(0); //переводим String в char
+
+        System.out.println("Вы ввели знак: " + sign);
+
+        System.out.println("Введите второе число");
+
+        numberTwo = read.nextInt();
+
+        System.out.println("Вы ввели число: " + numberTwo);
+
+        ReadNumber number = new ReadNumber(numberOne, numberTwo);
+
+        switch (v) {
+            case '+': System.out.println("Ответ: " + number.addition());
+                break;
+            case '-': System.out.println("Ответ: " + number.subtract());
+                break;
+        }
+    }
+
+}
+
---
+++ src/ReadNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReadNumber {
+    int one;
+    int two;
+
+    public ReadNumber(int numberOne, int numberTwo) {
+        one = numberOne;
+        two = numberTwo;
+    }
+
+    int addition() {
+
+        return one + two;
+
+    }
+
+    int subtract() {
+        return one - two;
+    }
+
+
+}
